0d8748d4993d46c330589376761d025dba1a53e9
Wijdan <wijdanbita@gmail.com>
2025-04-28 14:53:38 +0700

    update FE : css untuk warna custom

--- src/features/auth/pages/LoginPage.jsx
+++ src/features/auth/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import LoginForm from '../components/LoginForm';
 function LoginPage() {
   return (
     <div>
-      <h2 className='text-2xl bg-red-500'>Page Login Ini</h2>
+      <h2 className='text-2xl bg-lm-primary'>Page Login Ini</h2>
       <LoginForm />
     </div>
   );
